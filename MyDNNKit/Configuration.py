db0e5b33c6c857016fb0c16e0462f424cdd14a29
enricojr <sc_enricojr@yahoo.it>
2019-11-06 12:32:33 +0100

    script to read the file sizes after selection

--- MyDNNKit/Configuration.py
+++ MyDNNKit/Configuration.py
@@ -19,34 +19,34 @@ VariablesToPlot = [
 InputDNNVariables = [
 
     # MERGED
-#    ['NJETS','l1_eta','l2_eta','l2_phi','l1_phi','ll_pt','fat_jet_eta','fat_jet_phi','fat_jet_pt','fat_jet_E','MET','l1_pt'],
-#    ['NJETS','l1_eta','l2_eta','l2_phi','l1_phi','ll_pt','fat_jet_eta','fat_jet_phi','fat_jet_pt','fat_jet_E','MET','l1_e'],
-#    ['NJETS','l1_eta','l2_eta','l2_phi','l1_phi','ll_pt','fat_jet_eta','fat_jet_phi','fat_jet_pt','fat_jet_E','MET','l2_pt'],
-#    ['NJETS','l1_eta','l2_eta','l2_phi','l1_phi','ll_pt','fat_jet_eta','fat_jet_phi','fat_jet_pt','fat_jet_E','MET','l2_e'],
-#    ['NJETS','l1_eta','l2_eta','l2_phi','l1_phi','ll_pt','fat_jet_eta','fat_jet_phi','fat_jet_pt','fat_jet_E','MET','fat_jet_D2'],
-#    ['NJETS','l1_eta','l2_eta','l2_phi','l1_phi','ll_pt','fat_jet_eta','fat_jet_phi','fat_jet_pt','fat_jet_E','MET','fat_jet_C2'],
-#    ['NJETS','l1_eta','l2_eta','l2_phi','l1_phi','ll_pt','fat_jet_eta','fat_jet_phi','fat_jet_pt','fat_jet_E','MET','ratio_merged'],
-#    ['NJETS','l1_eta','l2_eta','l2_phi','l1_phi','ll_pt','fat_jet_eta','fat_jet_phi','fat_jet_pt','fat_jet_E','MET','ratio_resolved'],
-#    ['NJETS','l1_eta','l2_eta','l2_phi','l1_phi','ll_pt','fat_jet_eta','fat_jet_phi','fat_jet_pt','fat_jet_E','MET','fat_jet_ntrack']
+    ['NJETS','l1_eta','l2_eta','l2_phi','l1_phi','ll_pt','fat_jet_eta','fat_jet_phi','fat_jet_pt','fat_jet_E','MET','l1_pt'],
+    ['NJETS','l1_eta','l2_eta','l2_phi','l1_phi','ll_pt','fat_jet_eta','fat_jet_phi','fat_jet_pt','fat_jet_E','MET','l1_e'],
+    ['NJETS','l1_eta','l2_eta','l2_phi','l1_phi','ll_pt','fat_jet_eta','fat_jet_phi','fat_jet_pt','fat_jet_E','MET','l2_pt'],
+    ['NJETS','l1_eta','l2_eta','l2_phi','l1_phi','ll_pt','fat_jet_eta','fat_jet_phi','fat_jet_pt','fat_jet_E','MET','l2_e'],
+    ['NJETS','l1_eta','l2_eta','l2_phi','l1_phi','ll_pt','fat_jet_eta','fat_jet_phi','fat_jet_pt','fat_jet_E','MET','fat_jet_D2'],
+    ['NJETS','l1_eta','l2_eta','l2_phi','l1_phi','ll_pt','fat_jet_eta','fat_jet_phi','fat_jet_pt','fat_jet_E','MET','fat_jet_C2'],
+    ['NJETS','l1_eta','l2_eta','l2_phi','l1_phi','ll_pt','fat_jet_eta','fat_jet_phi','fat_jet_pt','fat_jet_E','MET','ratio_merged'],
+    ['NJETS','l1_eta','l2_eta','l2_phi','l1_phi','ll_pt','fat_jet_eta','fat_jet_phi','fat_jet_pt','fat_jet_E','MET','ratio_resolved'],
+    ['NJETS','l1_eta','l2_eta','l2_phi','l1_phi','ll_pt','fat_jet_eta','fat_jet_phi','fat_jet_pt','fat_jet_E','MET','fat_jet_ntrack']
 
     # RESOLVED
-    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','jj_j2pt'],
-    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','jj_j1M'],
-    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','jj_j1pt'],
-    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','l1_pt'],
-    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','l2_pt'],
-    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','l2_e'],
-    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','ratio_resolved'],
-    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','l1_e'],
-    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','jj_j2NTracks'],
-    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','jj_j1NTracks'],
-    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','jj_j2M'],
-    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','ratio_merged'],
-    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','detajj'],
-    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','jj_j1eta'],
-    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','jj_j2eta'],
-    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','jj_j2phi'],
-    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','jj_j1phi']   
+#    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','jj_j2pt'],
+#    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','jj_j1M'],
+#    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','jj_j1pt'],
+#    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','l1_pt'],
+#    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','l2_pt'],
+#    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','l2_e'],
+#    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','ratio_resolved'],
+#    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','l1_e'],
+#    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','jj_j2NTracks'],
+#    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','jj_j1NTracks'],
+#    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','jj_j2M'],
+#    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','ratio_merged'],
+#    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','detajj'],
+#    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','jj_j1eta'],
+#    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','jj_j2eta'],
+#    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','jj_j2phi'],
+#    ['NJETS','l2_eta','l1_eta','l1_phi','l2_phi','ll_pt','jj_eta','jj_phi','jj_pt','MET','jj_j1phi']   
         ]
 
 ScanParams = {
